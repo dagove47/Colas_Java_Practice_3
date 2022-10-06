d9e8e561030be4b585b1e8422448dcfac78ac269
David Gomez <dagove47@gmail.com>
2022-10-05 23:30:52 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Colas_Java_Practice_3
+ David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
---
+++ src/main/java/Colas_Java_Practice_3/Colas_Java_Practice_3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package Colas_Java_Practice_3;
+
+/**
+ *
+ * @author dagove47
+ */
+public class Colas_Java_Practice_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
