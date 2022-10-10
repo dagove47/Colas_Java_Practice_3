ca8684a9d6ecfe83a8b31cc63aa054fd355a28bd
alexkun90 <alexander9824@gmail.com>
2022-10-09 23:53:41 -0600

    Creacion de nodo
    nodoready

--- src/main/java/Colas_Java_Practice_3/Nodo.java
+++ src/main/java/Colas_Java_Practice_3/Nodo.java
@@ -1,4 +1,25 @@
 package Colas_Java_Practice_3;
 
 public class Nodo {
+        private String persona;
+        private String info;
+        private Nodo behind;
+        public Nodo() {
+        }
+        public String getInfo() {
+            return info;
+        }
+        public void setInfo(String info) {
+            this.info = info;
+        }
+        public Nodo getBehind() {
+            return behind;
+        }
+        public void setBehind(Nodo behind) {
+            this.behind = behind;
+        }
+        @Override
+        public String toString() {
+            return info;
+        }
 }
